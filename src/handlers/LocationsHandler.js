f2c41b2eda88d7f66b3670c0c2a76cfb63fede02
Firok <batefirok@yahoo.fr>
2019-12-25 15:05:12 +0000

    Adding and calling google map API

--- src/handlers/LocationsHandler.js
+++ src/handlers/LocationsHandler.js
@@ -1,9 +1,23 @@
+import googleMaps from "@google/maps"
+
+const googleMapsClient = googleMaps.createClient({
+  key: 'AIzaSyCqXEule4NLl0RTe7x51zvswqb-hPDSZYA',
+  Promise: Promise
+});
+
 const onPost = async (req, res, next) => {
   const { start, end, units } = req.body;
-  console.log(start);
-  console.log(end);
-  console.log(units);
-
+  
+  googleMapsClient.directions({
+    origin: start.lat + ',' + start.lng,
+    destination: end.lat + ',' + end.lng,
+    units: units
+  }).asPromise().then((response) => {
+    console.log(response.json);
+  })
+    .catch((err) => {
+      console.log(err);
+    });
 
   return res.json({
     message: 'Test me'
--- src/routes/v1.js
+++ src/routes/v1.js
@@ -2,16 +2,16 @@ import express from "express"
 import bodyParser from "body-parser"
 import LocationsHandler from  "../handlers/LocationsHandler"
 
-const router = express.Router()
+const router = express.Router();
 
 // Middleware
-router.use(bodyParser.json())
+router.use(bodyParser.json());
 
 /*
  * Routes
  */
 
 router.route("/get_distance_and_time")
-  .post(LocationsHandler.onPost)
+  .post(LocationsHandler.onPost);
 
 export default router
